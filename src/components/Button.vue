c9af3932905e82e6187831ef6312f87bfda0baf5
Makanin Yaroslav <fobosguychanel@gmail.com>
2026-07-02 14:35:14 +0300

    feat: button component

---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+    <button class="btn"><slot /></button>
+</template>
+
+<style scoped>
+.btn {
+  max-width: 335px;
+  width: 100%;
+  padding: 16px 10px;
+
+  font-family: var(--font);
+  font-size: 24px;
+  background-color: #008BFE;
+  color: white;
+
+  border: none;
+  border-radius: 100px;
+}
+</style>
